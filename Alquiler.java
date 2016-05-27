76a965b4be580601b1f49c5f7cd8f300ea05b592
Victor <victormel87@gmail.com>
2016-05-27 13:24:40 +0200

    Creada y completada clase Alquiler

---
+++ Alquiler.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Alquiler here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alquiler
+{
+    private int numeroDias;
+    private Cliente cliente;
+    private Barco barco;
+    private static final int VALOR_FIJO_ALQUILER = 300;
+    private static final int MULTIPLICADOR_ESLORA = 10;
+
+    /**
+     * Constructor for objects of class Alquiler
+     */
+    public Alquiler(int dias, Cliente cliente, Barco barco)
+    {
+        numeroDias = dias;
+        this.cliente = cliente;
+        this.barco = barco;
+    }
+
+    /**
+     * Devuelve el coste del alquiler del amarre
+     */
+    private float getCosteAlquiler(){
+        return numeroDias*(barco.getEslora()*MULTIPLICADOR_ESLORA) + (VALOR_FIJO_ALQUILER * barco.getCoeficienteBernua());
+    }
+    
+    /**
+     * Devuelve los datos del alquiler.
+     */
+    public String toString(){
+        return "\n---------------------------" +
+               "\n----- DATOS  ALQUILER -----" +
+               "\n---------------------------" + 
+               "\n\n" +
+               cliente + barco + "\nCoste Alquiler: " + getCosteAlquiler();
+               
+    }
+}
