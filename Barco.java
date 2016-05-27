797cc7335db9c3c924ad7129cf3c1e780bb4bee7
Victor <victormel87@gmail.com>
2016-05-27 12:43:08 +0200

    Creada y completada clase abstracta Barco

---
+++ Barco.java
@@ -0,0 +1,59 @@
+/**
+ * Abstract class Barco - write a description of the class here
+ * 
+ * @author: 
+ * Date: 
+ */
+public abstract class Barco
+{
+    private String matricula;
+    private float eslora;
+    private int anoFabricacion;
+
+    /**
+     * Contructor de barcos
+     */
+    public Barco(String matricula, float eslora, int anoFab){
+        this.matricula = matricula;
+        this.eslora = eslora;
+        anoFabricacion = anoFab;
+    }
+    
+    /**
+     * Devuelve la matricula del barco
+     */
+    public String getMatricula(){
+        return matricula;
+    }
+    
+    /**
+     * Devuelve la eslora del barco en metros
+     */
+    public float getEslora(){
+        return eslora;
+    }
+    
+    /**
+     * Devuelve el año de fabricacion del barco
+     */
+    public int getAnoFabricacion(){
+        return anoFabricacion;
+    }
+    
+    /**
+     * Metodo abstracto que devuelve el coeficiente de Bernua
+     */
+    public abstract int getCoeficienteBernua();
+    
+    /**
+     * Devuelve los datos del barco
+     */
+    public String toString(){
+        return "\n**************************" +
+               "\n*******DATOS  BARCO*******" +
+               "\n**************************" + 
+               "\nMatricula: " + matricula +
+               "\nEslora: " + eslora +
+               "\nAño Fabr: " + anoFabricacion;
+    }
+}
