a22d68d9ac6350692082465600af6b42a71bd7ff
Victor <victormel87@gmail.com>
2016-05-27 13:04:46 +0200

    Creada y completada la clase Velero

---
+++ Velero.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Velero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Velero extends Barco
+{
+    private int numeroMastiles;
+
+    /**
+     * Constructor for objects of class Velero
+     */
+    public Velero(int numeroMastiles, String matricula, float eslora, int anoFab)
+    {
+        super(matricula, eslora, anoFab);
+        this.numeroMastiles = numeroMastiles;
+    }
+    
+    /**
+     * Devuelve el coeficiente de Bernua del velero.
+     */
+    public int getCoeficienteBernua(){
+        return numeroMastiles;
+    }
+
+    /**
+     * Devuelve la informacion del velero. 
+     */
+    public String toString(){
+        String info;
+        info = super.toString() + "\nNum Mastiles: " + numeroMastiles;
+        return info;
+    }
+}
